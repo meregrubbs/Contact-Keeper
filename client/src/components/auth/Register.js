2c7c84593a99775eecb6ddcdd611fd29c6f13cd5
Meredith Grubbs <meregrubbs@gmail.com>
2020-10-10 10:12:08 -0600

    user registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,13 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import AuthContext from '../../context/auth/authContext';
 
 const Register = () => {
   const alertContext = useContext(AlertContext);
   const { setAlert } = alertContext;
+
+  const authContext = useContext(AuthContext);
+  const { register, error, clearErrors } = authContext;
 
   const [user, setUser] = useState({
     name: '',
@@ -14,16 +18,29 @@ const Register = () => {
 
   const { name, email, password, password2 } = user;
 
+  useEffect(() => {
+    if (error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+  }, [error]);
+
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
+    // Basic Error Checking
     if (name === '' || email === '' || password === '') {
       setAlert('Please enter all fields', 'danger');
     } else if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('Register submit');
+      // If all looks good, register
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
 
